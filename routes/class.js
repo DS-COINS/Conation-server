a864f8e5a429a05d79bd5fc30f1ec9cc98c9c133
unknown <lerle144@gmail.com>
2022-08-24 06:08:02 +0900

    /api/class/postClass 이미지 없는 경우

--- routes/class.js
+++ routes/class.js
@@ -1,7 +1,29 @@
 const express = require("express");
 const router = express.Router();
+const { Class } = require("../models/Class");
 
 
+router.post("/postClass", (req, res) => {
+    const newclass = new Class({
+      title: req.body.title,
+      placetype: req.body.placetype,
+      category: req.body.category,
+      content: req.body.content
+    });
+  
+    newclass
+      .save()
+      .then((result) => {
+        res.status(200).json({
+          success: true,
+          result: result,
+        });
+      })
+      .catch((err) => {
+        res.json({ success: false, err });
+      });
+  });
+  
 
 
 
