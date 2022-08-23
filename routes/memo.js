7eb5d9ceb1536ab0d9743cfd21fbb078a36d33ec
unknown <lerle144@gmail.com>
2022-08-24 04:34:43 +0900

    /api/memo/postMemo, getMemos

--- routes/memo.js
+++ routes/memo.js
@@ -1,9 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const { Memo } = require("../models/Memo");
 
+router.post("/postMemo", (req, res) => {
+  const memo = new Memo({
+    text: req.body.text,
+  });
 
+  memo
+    .save()
+    .then((result) => {
+      res.status(200).json({
+        success: true,
+        result: result,
+      });
+    })
+    .catch((err) => {
+      res.json({ success: false, err });
+    });
+});
 
+router.get("/getMemos", async (req, res, next) => {
+  try {
+    const page = req.query.page || 1;
+    const perPage = 8;
 
+    const memos = await Memo.find({})
+      .limit(perPage * 1)
+      .skip((page - 1) * perPage);
 
+    const total = await Memo.countDocuments({});
+    return res.status(200).json({
+      success: true,
+      memos,
+      totalPages: Math.ceil(total / perPage),
+      currentPage: page,
+    });
+  } catch (err) {
+    res.json({ success: false, err });
+    next(err);
+  }
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,28 +3,6 @@ const router = express.Router();
 const { User } = require("../models/User");
 
 const { auth } = require("../middleware/auth");
-
-router.get("/getUserInfo/:id", (req, res) => {
-  User.findOne({ _id: req.params.id })
-    .populate("bookmark")
-    .exec((err, user) => {
-      if (err) return res.json({ success: false, err });
-      StoreReview.find({ writer: req.params.id }).exec((err, reviews) => {
-        if (err) return res.json({ success: false, err });
-        StoreFavorite.find({ user: req.params.id })
-          .populate("store")
-          .exec((err, favorites) => {
-            if (err) return res.json({ success: false, err });
-            const userInfo = {
-              user: user,
-              reviews: reviews,
-              favorites: favorites,
-            };
-            res.status(200).json({ success: true, userInfo });
-          });
-      });
-    });
-});
   
 
 
