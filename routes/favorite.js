cb4197b696e16f78fc72efa69c1152958ed9a3cb
unknown <lerle144@gmail.com>
2022-08-25 00:48:08 +0900

    /api/favorite/postFavorite 찜하기 취소 기능 추가

--- routes/favorite.js
+++ routes/favorite.js
@@ -6,14 +6,36 @@ const { Class } = require("../models/Class");
 
 router.post("/postFavorite", async (req, res, next) => {
   try {
+
     const userId = req.body.userId
     const classId = req.body.classId
 
-    // Favorite 객체 생성 후 저장
+    /* 찜하기 취소 */
+    // 이미 Favorite 했는지 검사
+    const isExists = await Favorite.findOne(req.body)
+    if (isExists) {
+        // Favorite 콜렉션에서 제거
+        isExists.remove();
+
+        // 배열에서 삭제
+        await User.updateOne({_id:req.body.userId},
+            {$pull: {favorites: { $in: [classId]}}})
+        
+        await Class.updateOne({_id:req.body.classId},
+            {$pull: {favorites: userId}})
+
+        return res.status(200).json({
+            success: true
+        });
+    }
+
+    /* 찜하기 */
+
+    // Favorite 객체 생성 후 콜렉션에 저장
     const favorite = await Favorite(req.body);
     favorite.save()
     
-    
+    // 배열에 추가
     // User의 favorites 배열에 class를 푸시하고
     // Class의 favorites 배열에 userId를 푸시
     await User.updateOne({_id:req.body.userId},
@@ -23,7 +45,7 @@ router.post("/postFavorite", async (req, res, next) => {
         {$addToSet: {favorites: userId}})
     
     return res.status(200).json({
-      success: "true"
+      success: true
     });
   } catch (err) {
     res.json({ success: false, err });
